edc29f779657dc40f83cf2aa9e231bdaabd7801a
Juan Pascale <juan.pascale@sabf.org.ar>
2014-10-02 16:57:01 -0300

    Add game basic scaffolding

---
+++ GOG/src/ar/edu/itba/poo/main/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.poo.main;
+
+import ar.edu.itba.poo.gamelogic.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola.");
+
+	}
+
+}

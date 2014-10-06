19e15ac6b9d0972cd6a222042b23e0a7aef21278
temadu <teofiloduffau@gmail.com>
2014-10-06 17:39:22 -0300

    Add Warrior & Wizard class

---
+++ GOG/src/ar/edu/itba/poo/gamelogic/Warrior.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.poo.gamelogic;
+
+public class Warrior extends Strategy {
+
+	public Warrior() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ GOG/src/ar/edu/itba/poo/gamelogic/Wizard.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.poo.gamelogic;
+
+public class Wizard extends Strategy {
+
+	public Wizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
